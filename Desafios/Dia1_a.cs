bc63382d32dfcdaf2c1fe3b49e248bbb34aaec8d
Guilherme Weck <weckwerthguilherme@gmail.com>
2025-12-01 14:10:16 -0300

    Adicionar arquivos de projeto.

---
+++ Desafios/Dia1_a.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2025.Desafios
+{
+    public static class Dia1_a
+    {
+        public static void Desafio()
+        {
+            Console.WriteLine("OI");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using advent_of_code_2025.Desafios;
+
+namespace advent_of_code_2025_Desafios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dia1_a.Desafio(); 
+        }
+    }
+}
